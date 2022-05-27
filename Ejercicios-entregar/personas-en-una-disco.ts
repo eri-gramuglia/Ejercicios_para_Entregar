4014e1e99b1cc6fe40d883529eaed9c4840fbd9f
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-27 15:53:42 -0300

    Ejercicio Entregable 30/05/2022 Se corrigieron los errores de sintaxis y llamada de funciòn de carga de array

--- Ejercicios-entregar/personas-en-una-disco.ts
+++ Ejercicios-entregar/personas-en-una-disco.ts
@@ -1,29 +1,32 @@
 let edadClientesDisco: number[] = new Array(5); //array de 270-prueba de escritorio con 5 posiciones
 let index: number;
-let mayorIgual21: number=0;
-let menor21: number=0;
-let totalClientes: number=0;
+let mayorIgual21: number = 0;
+let menor21: number = 0;
+let totalClientes: number = 0;
 
-function getRamdonArbitrary (min,max){//funcion para generar números aleatorios.
-  return Math.floor(Math.random() * (max-min))+min;
+function getRamdonArbitrary(min, max: number) {
+  //funcion para generar números aleatorios.
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-function ingresoClientes (v:number[]){
-for (index = 0; index < 5; index++) {//llamado de la función de números aleatorios.
-  v[index]=getRamdonArbitrary (15,80); //carga de array con edades desde 15 hasta 80.
-  console.log(`Cliente Nº:edad ${index}, edad: ${ingresoClientes[index]}`);
+function ingresarClientes(v: number[]) {
+  //carga de array con edades desde 15 hasta 80.
+  for (index = 0; index <= 4; index++) {
+    v[index] = getRamdonArbitrary(15, 80); //llamado de la función de números aleatorios.
+    console.log(`Cliente Nº ${index}, edad: ${v[index]}`);
+  }
 }
-for (index = 0; index < 5; index++) {
-  ingresoClientes (edadClientesDisco);
-  if ((edadClientesDisco[index]<=18) && (edadClientesDisco[index]>=40)) {
-    console.log ("Prohibido su Ingreso");
-  } else if (edadClientesDisco[index]>=21) {
+ingresarClientes(edadClientesDisco);
+for (index = 0; index <= 4; index++) {
+  if (edadClientesDisco[index] >= 21 && edadClientesDisco[index] <= 40) {
     mayorIgual21++;
-  } else {
+  } else if (edadClientesDisco[index] >= 18 && edadClientesDisco[index] <= 21) {
     menor21++;
-  } 
+  } else {
+    console.log(`Cliente ${index} "Prohibido su Ingreso"`);
+  }
 }
 
-console.log("Hay",mayorIgual21,"personas entre 21 y 39 años.");
-console.log("Hay", menor21,"personas menores de 21 años.");
+console.log("Hay", mayorIgual21, "personas entre 21 y 40 años.");
+console.log("Hay", menor21, "personas menores de 21 años.");
 totalClientes = mayorIgual21 + menor21;
-console.log("En la Disco hay", totalClientes,"personas en total.");
+console.log(`En la Disco hay ${totalClientes} personas en total.`);

57ce1a9e852a574764087be106174fe4cbdebebc
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-06-02 14:16:56 -0300

    Ejercicio-cine

--- Ejercicios-entregar/cine.ts
+++ Ejercicios-entregar/cine.ts
@@ -1,18 +1,22 @@
-let dimensionArray: number = 5; //el ejercicio no especifica la cantidad de butacas.
-let vectorButacas: boolean[] = new Array(dimensionArray);
+let dimensionArray: number = Number(
+  prompt("Ingresar la cantidad de butacas del cine")
+); //el ejercicio no especifica la cantidad de butacas.
+let vectorButacas: boolean | number[] = new Array(dimensionArray);
 
-function cargarButacas(vector: boolean[], largo: number) {
-  for (let indice: number = 0; indice < largo; indice++)
-    vector[indice] = Math.floor(Math.random() * 2);
+function butacaBooleana() {
+  return Math.floor(Math.random() * 2);
 }
-function verificarDisponibilidad(vector: boolean[], largo: number) {
+function cargarButacas(vector: boolean | number[], largo: number) {
+  for (let indice: number = 0; indice < largo; indice++)
+    vector[indice] = butacaBooleana();
+}
+function verificarDisponibilidad(vector: boolean | number[], largo: number) {
   let butacaVacia: number = 0;
   for (let indice: number = 0; indice <= largo; indice++) {
-    if (vector[indice] == 0) butacaVacia++;
+    if (vector[indice] === 0) butacaVacia++;
   }
   console.log("La cantidad de butacas vacias es:", butacaVacia);
 }
-
 cargarButacas(vectorButacas, dimensionArray);
 console.log(vectorButacas); //sólo es para verificar la carga del vector
 verificarDisponibilidad(vectorButacas, dimensionArray);

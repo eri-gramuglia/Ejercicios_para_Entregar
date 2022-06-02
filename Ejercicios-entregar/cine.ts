7b155a8c348272f42d3274414ea9cf0316f2e721
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-06-01 23:39:41 -0300

    Ejercicio-cine

---
+++ Ejercicios-entregar/cine.ts
@@ -0,0 +1,18 @@
+let dimensionArray: number = 5;
+let vectorButacas: boolean[] = new Array(dimensionArray);
+
+function cargarButacas(vector: boolean[], largo: number) {
+  for (let indice: number = 0; indice < largo; indice++)
+    vector[indice] = Math.floor(Math.random() * 2);
+}
+function verificarDisponibilidad(vector: boolean[], largo: number) {
+  let butacaVacia: number = 0;
+  for (let indice: number = 0; indice <= largo; indice++) {
+    if (vector[indice] === 0) butacaVacia++;
+  }
+  console.log("La cantidad de butacas vacias es:", butacaVacia);
+}
+
+cargarButacas(vectorButacas, dimensionArray);
+console.log(vectorButacas); //sólo es para verificar la carga del vector
+verificarDisponibilidad(vectorButacas, dimensionArray);
